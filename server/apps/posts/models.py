7b93c1e204313af008d74b7a692a4630bc9b04b1
sudosubin <sudosubin@gmail.com>
2022-12-26 16:04:18 +0900

    feat: Add Post model

--- server/apps/posts/models.py
+++ server/apps/posts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, verbose_name="제목")
+    user = models.CharField(max_length=50, verbose_name="사용자 이름")
+    content = models.TextField(verbose_name="내용")
+    region = models.CharField(max_length=50, verbose_name="지역")
+    price = models.IntegerField(verbose_name="가격")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "post"
+        verbose_name = "게시글"
