fbf5c884780ffaff4713797fb62ca57a6df836be
sudosubin <sudosubin@gmail.com>
2022-12-26 17:10:40 +0900

    feat: Add templates

--- server/apps/posts/urls.py
+++ server/apps/posts/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from django.http import HttpResponse
 
 from server.apps.posts import views
 
 app_name = "posts"
 
 urlpatterns = [
-    path("health", lambda *args, **kwargs: HttpResponse("hi")),
+    path("", views.PostListView.as_view()),
+    path("post/create/", views.PostCreateView.as_view()),
+    path("post/<int:pk>/", views.PostDetailView.as_view()),
+    path("post/<int:pk>/update/", views.PostUpdateView.as_view()),
 ]
--- server/apps/posts/views.py
+++ server/apps/posts/views.py
@@ -1,3 +1,79 @@
-from django.shortcuts import render
+import traceback
 
-# Create your views here.
+from django.http import HttpRequest, HttpResponseRedirect
+from django.views.generic import TemplateView
+
+from server.apps.posts.models import Post
+
+
+class PostListView(TemplateView):
+    template_name = "posts/list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({"posts": Post.objects.all()})
+        return context
+
+
+class PostCreateView(TemplateView):
+    template_name = "posts/create.html"
+
+    def post(self, request: HttpRequest, *args, **kwargs):
+        Post.objects.create(
+            title=request.POST["title"],
+            user=request.POST["user"],
+            content=request.POST["content"],
+            region=request.POST["region"],
+            price=request.POST["price"],
+        )
+        return HttpResponseRedirect("/")
+
+
+class PostDetailView(TemplateView):
+    template_name = "posts/detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        post = Post.objects.get(id=kwargs["pk"])
+        context.update({"post": post})
+        return context
+
+    def get(self, request: HttpRequest, *args, **kwargs):
+        try:
+            return super().get(request, *args, **kwargs)
+        except Post.DoesNotExist as e:
+            traceback.print_exception(e)
+            return HttpResponseRedirect("/404")
+
+
+class PostUpdateView(TemplateView):
+    template_name = "posts/update.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        post = Post.objects.get(id=kwargs["pk"])
+        context.update({"post": post})
+        return context
+
+    def get(self, request: HttpRequest, *args, **kwargs):
+        try:
+            return super().get(request, *args, **kwargs)
+        except Post.DoesNotExist as e:
+            traceback.print_exception(e)
+            return HttpResponseRedirect("/404")
+
+    def post(self, request: HttpRequest, *args, **kwargs):
+        try:
+            post = Post.objects.get(id=kwargs["pk"])
+        except Post.DoesNotExist as e:
+            traceback.print_exception(e)
+            return HttpResponseRedirect("/")
+
+        post.title = request.POST["title"]
+        post.user = request.POST["user"]
+        post.content = request.POST["content"]
+        post.region = request.POST["region"]
+        post.price = request.POST["price"]
+        post.save()
+
+        return HttpResponseRedirect("/")
